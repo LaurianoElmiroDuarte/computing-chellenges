d917f1fb7efd4f06ff1343df19480bfcaba47c82
lauriano <laurianoelmiroduarte@gmail.com>
2020-09-26 18:56:14 -0300

    collection each_hash each_array map

---
+++ ruby/collections/each_hash.rb
@@ -0,0 +1,5 @@
+aulas = {'Aula 1': 'Liberada', 'Aula 2': 'Não liberada', 'Aula 3': 'Em breve', 'Aula 4': 'Liberada', 'Aula 5': 'Em Breve'}
+
+aulas.each do |key, value|
+    puts "#{key} #{value}"
+end
---
+++ ruby/collections/map.rb
@@ -0,0 +1,11 @@
+arei = [1, 2, 3, 4, 5, 6]
+
+novu_arei = arei.map do |a|
+    a * 2
+end
+
+puts "\n Array Original"
+puts "#{arei}"
+
+puts "\n Novo Array"
+puts "#{novu_arei}"
