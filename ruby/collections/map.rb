daceeaca5817d25ea964533bc3b5c3d5d3f91677
lauriano <laurianoelmiroduarte@gmail.com>
2020-09-26 19:12:28 -0300

    map and selec in ruby

--- ruby/collections/map.rb
+++ ruby/collections/map.rb
@@ -1,7 +1,7 @@
 arei = [1, 2, 3, 4, 5, 6]
 
 novu_arei = arei.map do |a|
-    a * 2
+    a * 3.7
 end
 
 puts "\n Array Original"
@@ -9,3 +9,12 @@ puts "#{arei}"
 
 puts "\n Novo Array"
 puts "#{novu_arei}"
+
+
+
+arei.map! do |b|
+    b * 3.14
+end
+
+puts "\n New Array"
+puts "#{arei}"
