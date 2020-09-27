bf3a64e4ce786b9d92c83503ad34a3ca4d98c88b
lauriano <laurianoelmiroduarte@gmail.com>
2020-09-27 11:37:14 -0300

    organize code

--- ruby/collections/select_hash.rb
+++ ruby/collections/select_hash.rb
@@ -1,4 +1,4 @@
-hash = {'0': 'zero', '1': 'um', '2': 'dois', '3': 'três', '4': 'quatro', '5': 'cinco', '6': 'seis'}
+hash = {0=> 'zero', 1=> 'um', 2=> 'dois', 3=> 'três', 4=> 'quatro', 5=> 'cinco', 6=> 'seis'}
 
 puts 'Selecionando keys com valores maior que 4'
 selection = hash.select do |key, value|
