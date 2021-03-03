207c2bf944eb73712cde9095fd55b447de63b240
Lauriano Elmiro Duarte <lauriano.silva@ihc.com.br>
2021-03-03 10:49:35 -0300

    create directory code and add scripts for habittus

--- python/habittus1.py
+++
@@ -1,9 +0,0 @@
-def print_header(str):
-    print ("+++%s+++" % str)
-
-print_header.category = 1
-print_header.text = "some info"
-
-
-print_header("%d %s" %  \
-(print_header.category, print_header.text))
