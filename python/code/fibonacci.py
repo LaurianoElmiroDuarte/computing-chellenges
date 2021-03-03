14720d1cdc0fc7fb7e42b12cf2a88229dde7ca2a
Lauriano Elmiro Duarte <lauriano.silva@ihc.com.br>
2021-03-03 18:55:13 -0300

    code fibonacci

--- python/code/fibonacci.py
+++ python/code/fibonacci.py
@@ -1,7 +1,7 @@
-def fib(x):    #escreve a série Fibonacci até n 
+def fib(x):    #escreve a série Fibonacci até x
     a, b = 0, 1
-    while a < x:
+    while a < x:  # valor não ultrapassa x
         print(a, end=' ')
         a, b = b, a + b
     print()
-    
+
